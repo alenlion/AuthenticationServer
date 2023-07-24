f36c7b82322f336469cf0e6db71bb54f02e54ac5
Rayan <Rayan.Aksu@gmail.com>
2023-07-24 16:25:44 +0300

    first commit

---
+++ src/test/java/com/kalem/authenticationserver/AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalem.authenticationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
